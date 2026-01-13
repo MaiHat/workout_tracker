3a637cc4916b34f22eeac61c6cd4af704818edf1
Mai Hattori <maiyakura@gmail.com>
2026-01-12 17:00:03 -0800

    Updates

--- src/components/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Route } from "react-router-dom";
-import { useAuth } from "../contexts/authContext";
-
-export default function PrivateRoute() {
-    const { currentUser } = useAuth();
-    return
-}
