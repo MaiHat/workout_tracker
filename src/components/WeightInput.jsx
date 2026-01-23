59db56b8a483cc2db96728b78e074cab4b3030b3
Mai Hattori <maiyakura@gmail.com>
2026-01-22 16:47:19 -0800

    kg/lbs

--- src/components/WeightInput.jsx
+++ src/components/WeightInput.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const KG_TO_LBS = 2.20462;
 
@@ -7,38 +7,46 @@ function floorTo1Decimal(num) {
 }
 
 export default function WeightInput({ index, weightUnit, formData, setFormData }) {
+ const weight = formData[index].weight;
+
   const handleWeightChange = (e) => {
     const value = e.target.value;
-    const num = value === "" ? "" :  Number(value);
+    const num = value === "" ? "" : Number(value);
 
     setFormData(prev => {
       const updated = [...prev];
-      if (weightUnit === "kg") {
       updated[index].weight = {
-        kg: num,
-        lbs: num === "" ? "" : floorTo1Decimal(num * KG_TO_LBS),
+        ...weight,
+        inputValue: num,
+        kg: weightUnit === "kg" ? num : num === "" ? "" : floorTo1Decimal(num / KG_TO_LBS),
+        lbs: weightUnit === "lbs" ? num : num === "" ? "" : floorTo1Decimal(num * KG_TO_LBS),
       };
-    } else {
-      updated[index].weight = {
-        lbs: num,
-        kg : num === "" ? "" : floorTo1Decimal(num / KG_TO_LBS),
-      };
-    }
-    return updated;
+      return updated;
     });
-  }
-    const weight = formData[index].weight
+  };
+   useEffect(() => {
+    setFormData(prev => {
+      const updated = [...prev];
+      const w = updated[index].weight;
+
+      if (w.inputValue === "") return updated;
+
+      updated[index].weight = {
+        ...w,
+        kg: weightUnit === "kg" ? w.inputValue : floorTo1Decimal(w.inputValue / KG_TO_LBS),
+        lbs: weightUnit === "lbs" ? w.inputValue : floorTo1Decimal(w.inputValue * KG_TO_LBS),
+      };
+      return updated;
+    });
+  }, [weightUnit]); // weightUnit が変わったら再計算
+
 
   return (
     <div className="weight-input">
       <input 
       type="number"
       placeholder='weight'
-      value={
-        weightUnit === "kg"
-       ? weight.kg
-        : weight.lbs
-      }
+      value={weight.inputValue ?? ""}
       onChange={handleWeightChange}
       />
       <span>{weightUnit}</span>
